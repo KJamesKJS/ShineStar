0155a07b5645a2c96a1513ea02e453cfecdc7d0f
KJames <k.james.dev@gmail.com>
2022-09-28 23:21:42 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <p>Welcome to my fisrt application.</p>
       </header>
     </div>
   );
